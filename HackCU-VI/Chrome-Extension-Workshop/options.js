17d772a7860ff34a8144634826a8de4cdceb1bca
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-02-22 14:37:09 -0700

    chrome extension workshop

---
+++ HackCU-VI/Chrome-Extension-Workshop/options.js
@@ -0,0 +1,6 @@
+let page = document.getElementById('buttonDiv');
+const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+function constructOptions(kButtonColors) {
+  
+}
+constructOptions(kButtonColors);
