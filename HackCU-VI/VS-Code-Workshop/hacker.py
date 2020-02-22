d487e716dc648f8327f8c33fd32d9e26827b6329
Matthew Strong <mast4878@colorado.edu>
2020-02-22 15:37:27 -0700

    fix

--- HackCU-VI/VS-Code-Workshop/hacker.py
+++ HackCU-VI/VS-Code-Workshop/hacker.py
@@ -1,5 +1,11 @@
 
 
 class Hacker():
-    pass
-    # to do!!
+    def __init__(self):
+        self.hackers = 1
+        # to do!!
+    def __call__(self):
+        pass
+
+    def __len__(self):
+        return self.hackers
